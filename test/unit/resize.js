2a8e5b5f1ee3269e214032f8142daf754781f7df
Steve Edson <steve@aiir.com>
2015-09-29 14:40:30 +0100

    changed to a pending test

--- test/unit/resize.js
+++ test/unit/resize.js
@@ -1,9 +1,6 @@
 var assert = require("assert");
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+describe('Resizing', function() {
+  describe('iOS', function () {
+    it('should create multiple resized assets from the template');
   });
 });
