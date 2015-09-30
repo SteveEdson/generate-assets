898f48de546ef38585901b8b6f108cf44a78fe30
Steve Edson <steve@aiir.com>
2015-09-30 10:26:22 +0100

    changed files around

--- test/unit/resize.js
+++ test/unit/resize.js
@@ -1,4 +1,6 @@
-var assert = require("assert");
+var assert = require("assert"),
+    ImageAssets = require('../../lib/image-assets');
+
 describe('Resizing', function() {
   describe('iOS', function () {
     it('should create multiple resized assets from the template');
