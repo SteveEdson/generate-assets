d91b400348dd6221d9f9ae4c0b42353b5ef58b99
Steve Edson <steve.edson@jaywing.com>
2015-10-20 16:56:54 +0100

    test(images): added test dependencies
    added 2 dependencies that will help write more extensive tests for the library

--- test/unit/resize.js
+++ test/unit/resize.js
@@ -1,6 +1,9 @@
 var assert = require("assert"),
     ImageAssets = require('../../lib/image-assets');
 
+// TODO: Write more tests using chai FS and or node image-size (https://github.com/netroy/image-size)
+
+
 describe('Resizing', function() {
     describe('iOS', function () {
         it('should create multiple resized assets from the template');
@@ -16,4 +19,3 @@ describe('Setup', function() {
         it('should create the necessary directory structure');
     });
 });
-
