c5f67d6a6f9ed932b781583ec3decd5b738def4c
Steve Edson <steve.edson@jaywing.com>
2015-10-21 10:00:10 +0100

    test(images): added a failing test
    added a failing test, ready to correct later

--- test/unit/resize.js
+++ test/unit/resize.js
@@ -1,12 +1,17 @@
 var assert = require("assert"),
-    ImageAssets = require('../../lib/image-assets');
+    ImageAssets = require('../../lib/image-assets'),
+    chai = require('chai');
+
+chai.use(require('chai-fs'));
 
 // TODO: Write more tests using chai FS and or node image-size (https://github.com/netroy/image-size)
 
 
 describe('Resizing', function() {
     describe('iOS', function () {
-        it('should create multiple resized assets from the template');
+        it('should create multiple resized assets from the template', function() {
+ 	    expect("../test.jpg").to.be.a.file("Not a file");
+	});
     });
 
     describe('Android', function () {
